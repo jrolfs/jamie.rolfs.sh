21c2ebe0ea285d3e4266dc85b0df27a7eed62df7
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-02-12 15:51:15 -0800

    docs(three): add CSS to make Storybook preview full screen

--- packages/three/.storybook/preview.ts
+++ packages/three/.storybook/preview.ts
@@ -1,5 +1,7 @@
 import { Parameters } from '@storybook/addons';
 
+import './preview.css';
+
 export const parameters: Parameters = {
   actions: { argTypesRegex: '^on(?!.*(Pointer|Wheel|Render))[A-Z].*' },
   controls: {
