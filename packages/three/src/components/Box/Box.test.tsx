17b0fe5a19829b51adfcd24f4356cb4c4d6c7865
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-02-13 00:36:17 -0800

    test(three): assert that `Box` rotates

--- packages/three/src/components/Box/Box.test.tsx
+++ packages/three/src/components/Box/Box.test.tsx
@@ -19,3 +19,17 @@ test('click event makes box bigger', async () => {
 
   expect(mesh.props.scale).toBe(1.5);
 });
+
+test('rotates', async () => {
+  const renderer = await ReactThreeTestRenderer.create(<Box />);
+  const mesh = renderer.scene.children[0];
+
+  const { x, y, z } = mesh.instance.rotation;
+
+  await renderer.advanceFrames(10, 10);
+
+  expect(mesh.instance.rotation.x).toBeGreaterThan(x);
+
+  expect(mesh.instance.rotation.y).toEqual(y);
+  expect(mesh.instance.rotation.z).toEqual(z);
+});
