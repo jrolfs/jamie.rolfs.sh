bad5dd091c779f4a0608515640c587421d147a3d
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-09-25 04:28:38 -0700

    test(three): set `IS_REACT_ACT_ENVIRONMENT`
    https://github.com/reactwg/react-18/discussions/102
    https://github.com/testing-library/react-testing-library/pull/994/files

--- packages/three/jest.setup.cjs
+++ packages/three/jest.setup.cjs
@@ -1,2 +1,4 @@
+global.IS_REACT_ACT_ENVIRONMENT = true;
+
 // eslint-disable-next-line import/no-extraneous-dependencies, global-require
 jest.mock('scheduler', () => require('scheduler/unstable_mock'));
