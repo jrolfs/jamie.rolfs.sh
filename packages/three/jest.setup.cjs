374e9c7e1b399ee935b7bd4d65a91b5035719794
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-11-22 23:28:48 -0800

    test(three): mock `useGLTF` globally

--- packages/three/jest.setup.cjs
+++ packages/three/jest.setup.cjs
@@ -1,4 +1,22 @@
+const { BufferGeometry, MeshStandardMaterial } = require('three');
+
 global.IS_REACT_ACT_ENVIRONMENT = true;
 
 // eslint-disable-next-line import/no-extraneous-dependencies, global-require
 jest.mock('scheduler', () => require('scheduler/unstable_mock'));
+
+jest.mock('@react-three/drei', () => {
+  const drei = jest.requireActual('@react-three/drei');
+  const useGLTF = jest.fn().mockReturnValue({
+    nodes: new Proxy({}, { get: () => ({ geometry: new BufferGeometry() }) }),
+    materials: new Proxy({}, { get: () => new MeshStandardMaterial() }),
+    preload: jest.fn(),
+  });
+
+  useGLTF.preload = jest.fn();
+
+  return {
+    ...drei,
+    useGLTF,
+  };
+});
