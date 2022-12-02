76a5912054ac938e53ba6762e91814c934c832ce
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-01 22:36:28 -0800

    feat: proof-of-concept for projecting to Macintosh screen
    I thought `<Html />` Drei would be the move, but I'm thinking what we really
    want to do is apply the image stream from the emulator as a texture.
    
    This POC programmatically updates the texture on the screen in a way that
    [should be fast enough](https://docs.pmnd.rs/react-three-fiber/advanced/pitfalls#%E2%9D%8C-setstate-in-fast-events-is-bad)
    for the VNC stream (in theory).

--- applications/site/src/components/application/Layout/Layout.tsx
+++ applications/site/src/components/application/Layout/Layout.tsx
@@ -1,8 +1,4 @@
-import {
-  MeshReflectorMaterial,
-  PresentationControls,
-  Stage,
-} from '@react-three/drei';
+import { PresentationControls, Stage } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { FunctionComponent, PropsWithChildren, Suspense } from 'react';
 
@@ -23,25 +19,9 @@ const Layout: FunctionComponent<PropsWithChildren<LayoutProps>> = ({
         speed={1.5}
         zoom={0.7}
       >
-        <Stage environment="warehouse" intensity={1}>
+        <Stage adjustCamera environment="warehouse" intensity={1}>
           {children}
         </Stage>
-        <mesh rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[170, 170]} />
-          <MeshReflectorMaterial
-            blur={[300, 100]}
-            color="#101010"
-            depthScale={1.2}
-            maxDepthThreshold={1.4}
-            metalness={0.5}
-            minDepthThreshold={0.4}
-            mirror={0}
-            mixBlur={1}
-            mixStrength={40}
-            resolution={2048}
-            roughness={1}
-          />
-        </mesh>
       </PresentationControls>
     </Suspense>
   </Canvas>
---
+++ packages/three/src/components/Macintosh/Macintosh.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const screen = style({
+  width: '720px',
+  height: '540px',
+  background: 'burlywood',
+});
+
+export const screenInner = style({
+  width: '100%',
+  height: '100%',
+});
