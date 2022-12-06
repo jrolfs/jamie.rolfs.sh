2f98895b62246a328617a7df042641ca4d1a4536
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-05 23:47:17 -0800

    build: add containerized emulator, hello world server
    github.com/jrolfs/docker-ubuntu-vnc

--- applications/emulator/src/index.ts
+++ applications/emulator/src/index.ts
@@ -1,2 +1,17 @@
-// eslint-disable-next-line no-console
-export const hello = () => console.info('hello world');
+import { configuration } from './configuration';
+import { fastify } from './server';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+fastify.get('/', async (_request, _reply) => ({ hello: 'mom' }));
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: configuration.port, host: '0.0.0.0' });
+  } catch (error) {
+    fastify.log.error(error);
+
+    process.exit(1);
+  }
+};
+
+void start();
