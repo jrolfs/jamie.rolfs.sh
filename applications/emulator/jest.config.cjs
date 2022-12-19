d097d45c7024ec2f5f3fac23f79837e07788ce1f
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-18 22:58:19 -0800

    test(emulator): load `.env.example` for running tests

--- applications/emulator/jest.config.cjs
+++ applications/emulator/jest.config.cjs
@@ -1,6 +1,11 @@
 /** @typedef {import('ts-jest/dist/types').InitialOptionsTsJest} JestConfig */
 
+const path = require('path');
+
 const config = require('../../jest/base.config.cjs');
 
 /** @type {JestConfig} */
-module.exports = config;
+module.exports = {
+  ...config,
+  setupFilesAfterEnv: [path.join(__dirname, 'jest.setup.cjs')],
+};
