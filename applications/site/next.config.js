1ee31944eac995e9fdfcc3bca4cba81b2f3a3025
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-18 22:52:29 -0800

    build(site): disable redundant ESLint that Next runs on build

--- applications/site/next.config.js
+++ applications/site/next.config.js
@@ -4,6 +4,8 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+};
 
 module.exports = withVanillaExtract(withPreconstruct(nextConfig));
