bb5242d7a9d885d3fe89391cd29b2f6b8a388c2c
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-12-19 16:09:13 -0800

    chore: best attempt at using specific Node version for future reference

--- flake.nix
+++ flake.nix
@@ -8,12 +8,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        # buildNodejs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> { python = pkgs.python3; };
-        # nodejs-project = buildNodejs {
-        #   enableNpm = true;
-        #   version = "18.12.1";
-        #   sha256 = "T6QGRRvFJlmikOUs/bIWKnYL1UnaS4u+vmop8pbZON8=";
-        # };
+
+        # Ideally I'd love to be able to use Nix _with_ a specific Node version
+        # specified in `.node-version`, which is what this does. The problem
+        # is, it takes _forever_ to compile Node. I guess we'll track whatever
+        # Node is in Nix with `.node-version` instead of the other way around.
+        # I'm going to leave this here as an example just in case.
+        buildNodejs = pkgs.callPackage <nixpkgs/pkgs/development/web/nodejs/nodejs.nix> { python = pkgs.python3; };
+        nodejs-project = buildNodejs {
+          enableNpm = true;
+          version = nixpkgs.lib.traceVal (nixpkgs.lib.strings.fileContents ./.node-version);
+          sha256 = "T6QGRRvFJlmikOUs/bIWKnYL1UnaS4u+vmop8pbZON8=";
+        };
       in
       {
         devShell = with pkgs; pkgs.mkShell {
@@ -23,6 +29,5 @@
             (yarn.override { nodejs = nodejs-18_x; })
           ];
         };
-
       });
 }
