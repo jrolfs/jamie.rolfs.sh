7522ecac290a27ee2b95a4fbf5bb0e22062e29e2
Jamie Rolfs <jamie.rolfs@gmail.com>
2023-01-01 23:21:07 -0800

    feat(site): add favicon

--- applications/site/src/pages/_document.tsx
+++ applications/site/src/pages/_document.tsx
@@ -5,6 +5,19 @@ import { configuration } from '../configuration';
 const Document = () => (
   <Html>
     <Head>
+      <link href="/favicon/apple.png" rel="apple-touch-icon" sizes="180x180" />
+      <link
+        href="/favicon/32x32.png"
+        rel="icon"
+        sizes="32x32"
+        type="image/png"
+      />
+      <link
+        href="/favicon/16x16.png"
+        rel="icon"
+        sizes="16x16"
+        type="image/png"
+      />
       <script
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{
