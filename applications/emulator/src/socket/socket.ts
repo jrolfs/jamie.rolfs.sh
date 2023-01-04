4740d9c47e3c48ec20d5f95243469ab953e3e91c
Jamie Rolfs <jamie.rolfs@gmail.com>
2023-01-04 03:07:36 -0800

    chore(emulator): add helper for starting Mini vMac

--- applications/emulator/src/socket/socket.ts
+++ applications/emulator/src/socket/socket.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify';
 
+import { start as startMac } from '../process/mini-vmac';
 import { start as startVNC } from '../process/x11vnc';
 import { start as startBuffer } from '../process/xvfb';
 
@@ -11,14 +12,17 @@ export const boot = (io: Server, { log }: FastifyInstance) => {
 
     try {
       const display = 0;
+
       const bufferProcess = await startBuffer({ display, log });
       const vncProcess = await startVNC({ display, log });
+      const macProcess = await startMac({ display, log });
 
       socket.on('disconnect', () => {
         log.debug({ id: socket.id }, 'socket: disconnect');
 
         bufferProcess.kill();
         vncProcess.kill();
+        macProcess.kill();
       });
     } catch (error) {
       log.debug(error, 'failed to start xfvb');
