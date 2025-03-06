8a148fbc6c44a7190dd213126e13b6c2b0ab6e8a
Jamie Rolfs <jamie.rolfs@gmail.com>
2025-03-05 23:51:31 -0800

    style(emulator): fix lints

--- packages/core/src/theme/colors.ts
+++ packages/core/src/theme/colors.ts
@@ -103,13 +103,13 @@ export const neutral = {
   900: '#161515',
 } as const;
 
-export type Green = typeof green[keyof typeof green];
-export type Yellow = typeof yellow[keyof typeof yellow];
-export type Orange = typeof orange[keyof typeof orange];
-export type Red = typeof red[keyof typeof red];
-export type Purple = typeof purple[keyof typeof purple];
-export type Blue = typeof blue[keyof typeof blue];
-export type Neutral = typeof neutral[keyof typeof neutral];
+export type Green = (typeof green)[keyof typeof green];
+export type Yellow = (typeof yellow)[keyof typeof yellow];
+export type Orange = (typeof orange)[keyof typeof orange];
+export type Red = (typeof red)[keyof typeof red];
+export type Purple = (typeof purple)[keyof typeof purple];
+export type Blue = (typeof blue)[keyof typeof blue];
+export type Neutral = (typeof neutral)[keyof typeof neutral];
 
 export type Color = Green | Yellow | Orange | Red | Purple | Blue | Neutral;
 
